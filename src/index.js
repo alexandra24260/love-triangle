19e948d1ad5e749243482b4d3ec96515ab2b78ec
Alexandra Vecher <alex192004@inbox.ru>
2019-10-15 13:21:51 +0300

    added solution to task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    let triangle = preferences;
+    for ( let i = 0; i < triangle.length; i++) {
+         let searchPosition = triangle[ i];
+         let secondSearchPosition = triangle[searchPosition - 1];
+         let thirdSearchPosition = triangle[secondSearchPosition -1];
+         if ( thirdSearchPosition === i + 1){
+             count ++;
+         }
+    }
+    return Math.floor(count/3);
 };
